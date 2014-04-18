66e3731d3a5635938d9b18a6e2baa783894d38e7
Abundnce10 <michaelstitt@gmail.com>
2014-04-17 21:48:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='socialanalytics',
-    version='0.1.0',
+    version='0.2.0',
     author='Michael Stitt',
     author_email='michaelstitt10@hotmail.com',
     packages=['socialanalytics'],
