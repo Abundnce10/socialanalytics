035c3ad4fb49bfbdc72f336fe54951734bef09d9
Abundnce10 <michaelstitt@gmail.com>
2014-05-06 20:42:16 -0700

    Change README and setup file to work with PyPi

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='socialanalytics',
-    version='0.2.0',
+    version='0.2.1',
     author='Michael Stitt',
     author_email='michaelstitt10@hotmail.com',
     packages=['socialanalytics'],
     url='https://github.com/Abundnce10/socialanalytics',
     license='LICENSE.txt',
     description='Insight into how a specific URL performs on a variety of social networks.',
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[
         "requests >= 2.0.0",
     ],
